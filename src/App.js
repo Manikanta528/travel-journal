c1dda0624e5383d1d03ac516aa9c6dc346eb8235
Manikanta528 <chmanikanta528@gmail.com>
2022-10-31 19:51:44 +0530

    nav component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
  
 const App = () => {
     return(
-        <div className="app">Hello React</div>
+        <div className="app">
+            <Navbar />
+        </div>
     )
 }
 
