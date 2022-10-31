195520dfa4681ee28586a1a8a1fb12d02bdce1cb
Manikanta528 <chmanikanta528@gmail.com>
2022-10-31 21:51:37 +0530

    card component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Card from "./Components/Card";
 import Navbar from "./Components/Navbar";
+import data from "./data";
  
 const App = () => {
+
+    const cards = data.map( (card,index) => (<div><Card key = {index} item = {card}/><hr/></div>));
+
     return(
         <div className="app">
             <Navbar />
+            {cards}
         </div>
     )
 }
